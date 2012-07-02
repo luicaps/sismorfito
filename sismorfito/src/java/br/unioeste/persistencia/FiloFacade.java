5a4908c9a126513bab142b4192579c6f0946884d
luizgustavodesouza@gmail.com <luizgustavodesouza@gmail.com@085deb20-e958-199e-3435-638e1b96d5cb>
2012-07-02 11:58:55 +0000

    Cadastra Fito em andamento

--- sismorfito/src/java/br/unioeste/persistencia/FiloFacade.java
+++ sismorfito/src/java/br/unioeste/persistencia/FiloFacade.java
@@ -18,6 +18,7 @@ import javax.persistence.TypedQuery;
  */
 @Stateless
 public class FiloFacade extends AbstractFacade<Filo> {
+
 	@PersistenceContext(unitName = "sismorfitoPU")
 	private EntityManager em;
 
@@ -29,17 +30,20 @@ public class FiloFacade extends AbstractFacade<Filo> {
 	public FiloFacade() {
 		super(Filo.class);
 	}
-	
-	public List<Classe> findClassesFromFilo(String nomefilo){
+
+	public List<Classe> findClassesFromFilo(String nomefilo) {
 
 		TypedQuery<Filo> q = getEntityManager().createQuery("select f from Filo f where f.nomeFilo =:arg1", Filo.class).setParameter("arg1", nomefilo);
 
 		q.setMaxResults(1);
-		
+
 		List<Filo> lista = q.getResultList();
-		
+
+		if (lista == null || lista.get(0).getClasseList() == null) {
+			return null;
+		}
+
 		return lista.get(0).getClasseList();
-		
+
 	}
-	
 }
